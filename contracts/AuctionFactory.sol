2abe8ec4af342504fdb24e85f17d54083e0bb9b5
Victor Han <sunmoon11@yandex.ru>
2022-04-29 06:50:46 +0900

    Modified Auction.

---
+++ contracts/AuctionFactory.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract AuctionFactory is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    //static
+    Counters.Counter private _auctionIds;
+
+    IERC721 public immutable nft;
+    enum State {Created, Started, Canceled, Resolved}
+
+    struct Auction {
+        uint auctionId;
+        address payable seller;
+        uint tokenId;
+        uint duration;
+        uint reservePrice;
+        uint bidIncrement;
+        State state;
+        uint startAt;
+        uint expiresAt;
+        address highestBidder;
+    }
+
+    mapping(uint => Auction) private auctions;
+    mapping(uint => mapping(address => uint)) private balanceOfs;
+
+    event AuctionCreated(
+        uint auctionId,
+        address payable seller,
+        uint tokenId,
+        uint duration,
+        uint reservePrice,
+        uint bidIncrement
+    );
+    event AuctionStarted(uint auctionId);
+    event AuctionResolved(uint auctionId);
+    event AuctionCanceled(uint auctionId);
+    event Bid(uint auctionId, address bidder, uint bid);
+
+    constructor(address _nft) {
+        nft = IERC721(_nft);
+    }
+
+    function createAuction(
+        uint _tokenId,
+        uint _duration,
+        uint _reservePrice,
+        uint _bidIncrement
+    ) public payable nonReentrant {
+        require(nft.ownerOf(_tokenId)==msg.sender, "You have no this token.");
+        _auctionIds.increment();
+        uint auctionId = _auctionIds.current();
+
+        Auction storage auction = auctions[auctionId];
+        auction.auctionId = auctionId;
+        auction.seller = payable(msg.sender);
+        auction.tokenId = _tokenId;
+        auction.duration = _duration;
+        auction.reservePrice = _reservePrice;
+        auction.bidIncrement = _bidIncrement;
+        //for the first time to compare with highestBid
+        balanceOfs[auctionId][address(0)] = _reservePrice;
+
+        emit AuctionCreated(
+            auctionId,
+            payable(msg.sender),
+            _tokenId,
+            _duration,
+            _reservePrice,
+            _bidIncrement
+        );
+    }
+
+    function withdraw(uint _auctionId) public {
+        Auction storage auction = auctions[_auctionId];
+        require(msg.sender != auction.highestBidder, "Your funds are locked!");
+
+        uint amount = balanceOfs[_auctionId][msg.sender];
+        if (amount > 0) {
+            balanceOfs[_auctionId][msg.sender] = 0;
+            payable(msg.sender).transfer(amount);
+        }
+    }
+
+    function bid(uint _auctionId) public
+    payable
+    nonReentrant
+    returns (bool success)
+    {
+        Auction storage auction = auctions[_auctionId];
+        require(auction.state != State.Canceled);
+        require(auction.seller != payable(msg.sender));
+        uint newBid = balanceOfs[_auctionId][msg.sender] + msg.value;
+        uint highestBid = balanceOfs[_auctionId][auction.highestBidder];
+
+        if (auction.state == State.Created) {
+            auction.state = State.Started;
+            emit AuctionStarted(_auctionId);
+
+            auction.startAt = block.timestamp;
+            auction.expiresAt = block.timestamp + auction.duration;
+
+            //lock seller's NFT until it's finished or cancelled.
+            IERC721(nft).transferFrom(auction.seller, address(this), auction.tokenId);
+
+            //First bid starts without bidIncrement
+            highestBid -= (highestBid * auction.bidIncrement) / 100;
+        }
+
+        // If the bid is not higher, send the
+        // money back (the revert statement
+        // will revert all changes in this
+        // function execution including
+        // it having received the money).
+        require(newBid >= highestBid + (highestBid * auction.bidIncrement) / 100, "Insufficient fund");
+        require(auction.startAt <= block.timestamp && block.timestamp <= auction.expiresAt);
+
+        balanceOfs[_auctionId][msg.sender] = newBid;
+        if (msg.sender != auction.highestBidder) {
+            auction.highestBidder = msg.sender;
+        }
+        emit Bid(_auctionId, msg.sender, newBid);
+        return true;
+    }
+
+    function resolve(uint _auctionId) public {
+        Auction storage auction = auctions[_auctionId];
+        require(auction.state == State.Started && block.timestamp >= auction.expiresAt, "Auction was not started.");
+
+        // transfer tokens to highest bidder
+        nft.transferFrom(address(this), auction.highestBidder, auction.tokenId);
+
+        // transfer ether balance to seller
+        balanceOfs[_auctionId][auction.seller] += balanceOfs[_auctionId][auction.highestBidder];
+        balanceOfs[_auctionId][auction.highestBidder] = 0;
+
+        auction.highestBidder = address(0);
+        auction.state = State.Resolved;
+        emit AuctionResolved(_auctionId);
+    }
+
+    function cancelAuction(uint _auctionId) public
+    returns (bool success)
+    {
+        Auction storage auction = auctions[_auctionId];
+        require(payable(msg.sender) == auction.seller, "You are not a seller.");
+        require(auction.state == State.Created, "You can't cancel this auction.");
+        auction.state = State.Canceled;
+        emit AuctionCanceled(_auctionId);
+        return true;
+    }
+
+    function balanceOf(uint _auctionId, address bidder) public view returns (uint){
+        return balanceOfs[_auctionId][bidder];
+    }
+
+    function fetchAuction(uint _auctionId) public view returns (Auction memory){
+        Auction storage currentAuction = auctions[_auctionId];
+        return currentAuction;
+    }
+
+    /* Returns only auctions that a user has created */
+    function fetchAuctions(address _seller) public view returns (Auction[] memory) {
+        uint totalAuctionCount = _auctionIds.current();
+        uint auctionCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalAuctionCount; i++) {
+            if (_seller == address(0) || auctions[i + 1].seller == payable(_seller)) {
+                auctionCount += 1;
+            }
+        }
+
+        Auction[] memory _auctions = new Auction[](auctionCount);
+        for (uint i = 0; i < totalAuctionCount; i++) {
+            if (_seller == address(0) || auctions[i + 1].seller == payable(_seller)) {
+                Auction storage currentAuction = auctions[i + 1];
+                _auctions[currentIndex] = currentAuction;
+                currentIndex += 1;
+            }
+        }
+        return _auctions;
+    }
+}
---
+++ test/AuctionFactory.test.ts
@@ -0,0 +1,187 @@
+// test/AuctionFactory.test.ts
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { BigNumber, Contract, Signer } from "ethers";
+import { delay } from "@nomiclabs/hardhat-etherscan/dist/src/etherscan/EtherscanService";
+
+describe("AuctionFactory", function () {
+  let auctionFactory: Contract;
+  let nft: Contract;
+  let owner: Signer;
+  let user1: Signer;
+  let user2: Signer;
+
+  enum State {
+    Created,
+    Started,
+    Canceled,
+    Resolved,
+  }
+
+  beforeEach(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
+    const AuctionFactoryToken = await ethers.getContractFactory(
+      "AuctionFactory"
+    );
+    const NFTToken = await ethers.getContractFactory("NFTToken");
+    nft = await NFTToken.deploy();
+    await nft.safeMint(await owner.getAddress());
+    await nft.safeMint(await user1.getAddress());
+    auctionFactory = await AuctionFactoryToken.deploy(nft.address);
+    await auctionFactory.createAuction(
+      0,
+      3, // 3s
+      ethers.utils.parseEther("1"),
+      5 // 5%
+    );
+    await nft.setApprovalForAll(auctionFactory.address, true);
+  });
+
+  it("Should set the right and check balance of seller", async function () {
+    // Expect receives a value, and wraps it in an Assertion object. These
+    // objects have a lot of utility methods to assert values.
+
+    // This test expects the seller variable stored in the contract to be equal
+    // to our Signer's owner.
+    const auctions = await auctionFactory.fetchAuctions(
+      await owner.getAddress()
+    );
+    expect(auctions[0].seller).to.equal(await owner.getAddress());
+    expect(
+      await auctionFactory.balanceOf(
+        auctions[0].auctionId,
+        await owner.getAddress()
+      )
+    ).to.equal(BigNumber.from(0));
+  });
+
+  it("Should bid test.", async () => {
+    const auctions = await auctionFactory.fetchAuctions(
+      await owner.getAddress()
+    );
+    // Checking first bid
+    expect(auctions[0].state).to.equal(State.Created);
+    await expect(
+      auctionFactory
+        .connect(user1)
+        .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("0.5") })
+    ).to.be.revertedWith("Insufficient fund");
+    expect(
+      await auctionFactory.balanceOf(
+        auctions[0].auctionId,
+        await user1.getAddress()
+      )
+    ).to.equal(BigNumber.from(0));
+    await auctionFactory
+      .connect(user1)
+      .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("1") });
+    expect(
+      (await auctionFactory.fetchAuction(auctions[0].auctionId)).state
+    ).to.equal(State.Started);
+    expect(
+      await auctionFactory.balanceOf(
+        auctions[0].auctionId,
+        await user1.getAddress()
+      )
+    ).to.equal(ethers.utils.parseEther("1"));
+
+    // Checking second bid and highest bidder
+    await expect(
+      auctionFactory
+        .connect(user2)
+        .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("1.04") })
+    ).to.be.revertedWith("Insufficient fund");
+    await auctionFactory
+      .connect(user2)
+      .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("1.05") });
+    expect(
+      (await auctionFactory.fetchAuction(auctions[0].auctionId)).highestBidder
+    ).to.equal(await user2.getAddress());
+  });
+
+  it("Should withdraw test.", async () => {
+    const auctions = await auctionFactory.fetchAuctions(
+      await owner.getAddress()
+    );
+    await auctionFactory
+      .connect(user1)
+      .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("1") });
+    await auctionFactory
+      .connect(user2)
+      .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("1.05") });
+    await expect(
+      auctionFactory.connect(user2).withdraw(auctions[0].auctionId)
+    ).to.be.revertedWith("Your funds are locked!");
+    await auctionFactory.connect(user1).withdraw(auctions[0].auctionId);
+    expect(
+      await auctionFactory.balanceOf(
+        auctions[0].auctionId,
+        await user1.getAddress()
+      )
+    ).to.equal(BigNumber.from(0));
+  });
+
+  it("Should resolve test.", async () => {
+    const auctions = await auctionFactory.fetchAuctions(
+      await owner.getAddress()
+    );
+    await expect(
+      auctionFactory.resolve(auctions[0].auctionId)
+    ).to.be.revertedWith("Auction was not started.");
+    await auctionFactory
+      .connect(user1)
+      .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("1") });
+    await auctionFactory
+      .connect(user2)
+      .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("1.05") });
+    await delay(4000);
+    expect(await nft.balanceOf(await user2.getAddress())).to.equal(
+      BigNumber.from(0)
+    );
+    await auctionFactory.connect(user2).resolve(auctions[0].auctionId);
+    expect(
+      (await auctionFactory.fetchAuction(auctions[0].auctionId)).state
+    ).to.equal(State.Resolved);
+    expect(await nft.balanceOf(await user2.getAddress())).to.equal(
+      BigNumber.from(1)
+    );
+  });
+  it("Should cancel test.", async () => {
+    const auctions = await auctionFactory.fetchAuctions(
+      await owner.getAddress()
+    );
+    await expect(
+      auctionFactory.connect(user1).cancelAuction(auctions[0].auctionId)
+    ).to.be.revertedWith("You are not a seller.");
+    await auctionFactory
+      .connect(user1)
+      .bid(auctions[0].auctionId, { value: ethers.utils.parseEther("1") });
+    await expect(
+      auctionFactory.cancelAuction(auctions[0].auctionId)
+    ).to.be.revertedWith("You can't cancel this auction.");
+
+    await auctionFactory.connect(user1).createAuction(
+      1,
+      3, // 3s
+      ethers.utils.parseEther("1"),
+      5 // 5%
+    );
+    const newAuctions = await auctionFactory.fetchAuctions(
+      await user1.getAddress()
+    );
+    await auctionFactory.connect(user1).cancelAuction(newAuctions[0].auctionId);
+    expect(
+      (await auctionFactory.fetchAuction(newAuctions[0].auctionId)).state
+    ).to.equal(State.Canceled);
+  });
+  it("Should create auction test.", async () => {
+    await expect(
+      auctionFactory.connect(user2).createAuction(
+        1,
+        3, // 3s
+        ethers.utils.parseEther("1"),
+        5 // 5%
+      )
+    ).to.be.revertedWith("You have no this token.");
+  });
+});
